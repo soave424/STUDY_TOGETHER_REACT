1473e6d8e44eeebdfdfa069dae9b0f20d2489d38
soave424 <soave424@gmail.com>
2023-04-06 22:34:15 +0900

    week2 add

--- 죠이/react-for-beginners/src/App.js
+++ 죠이/react-for-beginners/src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 
 function App() {
-  const [counter, setCounter] = React.useState(0);
+  const [counter, setCounter] = React.useState(1);
   const [number, setNumber] = React.useState();
   const [divisor, setDivisor] = React.useState([]);
+
   const onClick = () => {
-    if (number > counter) {
+    if (number >= counter) {
       setCounter(counter + 1);
-    }
-    if (number % counter === 0) {
-      setDivisor([...divisor, counter]);
+      if (number % counter === 0) {
+        setDivisor([...divisor, counter.toString()]);
+      }
     }
   };
   const onChange = (e) => {
     setNumber(e.target.value);
     setCounter(1);
+    setDivisor([]);
   };
   return (
     <div>
@@ -23,11 +25,11 @@ function App() {
       <input onChange={onChange}></input>
       <h3>2. 나누어지는 수를 찾아 봅시다. </h3>
       <h2>
-        {number} ÷ {counter} = {number / counter}{" "}
+        {number} ÷ {counter} = {parseInt(number / counter)}...{number % counter}
       </h2>
       <button onClick={onClick}>Click me</button>
       <h3>3. 약수를 적어 봅시다.</h3>
-      <h3>{divisor}</h3>
+      <h3>{divisor.join(",")}</h3>
     </div>
   );
 }
